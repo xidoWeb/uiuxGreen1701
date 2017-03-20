ff3ef7a67c864d515b8a50c8d1e0317089183d19
xido <erivery2@gmail.com>
2017-03-20 16:23:01 +0900

    gallery_02

--- www/step_01.jquery_basic/js/src/imageGallery.js
+++ www/step_01.jquery_basic/js/src/imageGallery.js
@@ -30,14 +30,40 @@
   // console.log(gallery.thum[0].src);
   // console.log(gallery.thum[0].alt);
   
-  // 반복 효과로 찾아내기
+  // 반복 효과로 찾아내기, thum 안의 갯수'length' 파악하기
+  // console.log(gallery.thum.length);
+  var imgThum = $('#imgThum');
   
   // JSON형식의 이미지를 #imgThum 내부 li에 각각 할당
+  var i = 0;
+  for(; i < gallery.thum.length; i++){
+//    console.log(gallery.thum[i].src + ' : ' +gallery.thum[i].alt); // 갯수검증
+    imgThum.append('<li><a href="#"><img></a></li>');
+    var liEq = imgThum.find('li').eq(i).find('img');
+    
+      liEq.attr('src', (thumUrl + gallery.thum[i].src) )
+      liEq.attr('alt', (thumUrl + gallery.thum[i].alt) );
+  }
+  
+  
+  
   
   // li(이미지) 클릭했을경우 
-  
+  imgThum.find('li').on('click', function(e){
+    e.preventDefault();
+    // 선택한 이미지의 위치확인 및 연결된 큰이미지(JSON에서) 찾기
+      // 클릭한 이미지 리스트 순번 찾기
+    var imgIndex = $(this).index(); 
   // 선택된 이미지의 큰이미지를 #imgBox내부에 img코드를 생성후 삽입
+    var imgBox = $('#imgBox');
+    imgBox.html('<img>');
+    imgBox.find('img').attr('src', (bigUrl + gallery.big[ imgIndex ].src));
+    imgBox.find('img').attr('alt', (bigUrl + gallery.big[ imgIndex ].alt));
   
   // 별도의 코드를 이용해 이미지 설명글을 보이게 첨부
+  });
+  
+  
+  
   
 })(this.jQuery);
