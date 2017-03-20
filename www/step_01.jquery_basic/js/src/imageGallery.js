fe8a00d3db774ddec931db6ce7167aa157abb789
xido <erivery2@gmail.com>
2017-03-20 14:43:59 +0900

    galleryImg check_step_02

--- www/step_01.jquery_basic/js/src/imageGallery.js
+++ www/step_01.jquery_basic/js/src/imageGallery.js
@@ -1,5 +1,16 @@
 //imageGallery.js
-
-(function($){
+(function ($) {
+  // 이미지 경로 변수할당
+  var thumUrl, bigUrl;
+  
+  // 각각의 이미지파일들  JSON형식으로 작성
+  
+  // JSON형식의 이미지를 #imgThum 내부 li에 각각 할당
+  
+  // li(이미지) 클릭했을경우 
+  
+  // 선택된 이미지의 큰이미지를 #imgBox내부에 img코드를 생성후 삽입
+  
+  // 별도의 코드를 이용해 이미지 설명글을 보이게 첨부
   
 })(this.jQuery);
