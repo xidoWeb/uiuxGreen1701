88edfc0c3c363a1581eac1c31089515900c44115
xido <erivery2@gmail.com>
2017-04-26 14:24:00 +0900

    slideBanner_01 step3: button click animate

--- www/step_02.jquery_exam/js/src/slideBanner_01.js
+++ www/step_02.jquery_exam/js/src/slideBanner_01.js
@@ -13,7 +13,45 @@
   bannerBox.css({marginLeft: -100 + '%'});
   // banner 넘침 숨김
   // banner.css({overflow:'hidden'});
+ // ----------------------------------------------------- 
+  var lbtn = $('.lbtn');
+  var rbtn = $('.rbtn');
+  var timed = 1000; 
+  //http://easings.net/ko // jquery easing 내용위치
+  var ef = 'easeOutBack'; 
+  // lbtn 클릭시 작동하는 내용처리
+  lbtn.on('click', function(){
+    // li순서가 변경되어있기 때문에 마지막을 재정리해서 liLast에 저장
+    liLast = bannerUl.find('li').last();
+    // 애니메이션 처리
+    bannerBox.stop().animate({marginLeft:0}, timed, ef, function(){
+      // 애니메이션 처리 후 재배치 
+      bannerUl.prepend(liLast);
+      bannerBox.css({marginLeft:-100 + '%'});
+    });
+  }); // lbtn.on(click)
+  // rbtn 클릭시 작동하는 내용처리
+  rbtn.on('click', function(){
+    // li순서가 변경되어있기 때문에 마지막을 재정리해서 liLast에 저장
+    var lifirst = bannerUl.find('li').first();
+    // 애니메이션 처리
+    bannerBox.stop().animate({marginLeft:-200 +'%'}, timed, ef, function(){
+      // 애니메이션 처리 후 재배치 
+      bannerUl.append(lifirst);
+      bannerBox.css({marginLeft:-100 + '%'});
+    });
+  }); // lbtn.on(click)
   
   
   
 })(this.jQuery);
+
+
+
+
+
+
+
+
+
+
