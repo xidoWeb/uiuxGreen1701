dbca339960c4a7d26436603edda7bfccec5777c0
xido <erivery2@gmail.com>
2017-04-27 17:30:19 +0900

    adBanner_02 step_03 indicate clone and indicate click

--- www/step_02.jquery_exam/js/src/banner_02.js
+++ www/step_02.jquery_exam/js/src/banner_02.js
@@ -6,6 +6,7 @@
   각각을 담은 (bannerUl)
   배너 li (bannerLi)
   인디케이트 상자(indiUl)
+  첫번째 인디케이트(indiFir)
   마지막 배너(lastAd)
   마지막 인디케이트(lastInd)
   시간설정(timed)
@@ -44,6 +45,43 @@
   // bannerUl 왼쪽으로 한칸 이동
   bannerUl.css('marginLeft', '-100%');
 
+  // -------------------------------------
+  /* 인디케이트 상태를 배너와 동일하게 복제 처리
+  1. 변수 내용 체크
+    indiUl   = adBanner.find('.indi'),
+    indiFir  = indiUl.children('li').first(),
+    lastInd  = indiUl.children('li').last(),
+  2. 인디케이트 복제
+  3. 복제된 인디케이트는 보이지 않게 처리
+  */
+  // 인디케이트 복제
+  lastInd.clone().prependTo(indiUl);
+  // 복제된 첫 인디케이트 처리
+  var indiFir  = indiUl.children('li').first();
+  // console.log(indiFir.text()); // 첫번째 내용 확인
+  indiFir.css('display','none');  
+  // 갯수변동 여부 확인
+  // console.log(indiUl.children('li').length);
+  
+  // ------------------------------------------------
+  /* 인디케이트 클릭시 배너 이동
+  */
+  // 변경된 인디케이트 변수처리
+  var indiLi = indiUl.children('li');
+  // 인디케이트 클릭시 
+  indiLi.on('click', function(e){
+    // 이벤트 삭제
+    e.preventDefault();
+  // 해당하는 인디케이트의 순서를 파악
+    var $this = $(this);
+    var num = $this.index();
+   // console.log(num); // 순서 확인 검증
+    
+  // 해당순서의 배너가 화면에 위치하도록 처리(margin-left)
+    bannerUl.animate({marginLeft:-100 * num +'%'}, timed);
+    indiLi.removeClass('active');
+    $this.addClass('active',timed/3);
+  });
   
   
   
